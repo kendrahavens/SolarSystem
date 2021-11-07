a7c80b74687ee6dc1d2cca261585d9318d373457
Kendra Havens <kehavens@microsoft.com>
2021-11-07 13:24:33 -0800

    prep before .NET Conf talk

--- XUnitTests/OSInformation.cs
+++ XUnitTests/OSInformation.cs
@@ -2,9 +2,18 @@
 using System.Runtime.InteropServices;
 
 using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 public class RunningOnOS
 {
+    private readonly ITestOutputHelper output;
+
+    public RunningOnOS(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
     private bool InWindows { get { return Environment.OSVersion.VersionString.Contains("Win"); } }
 
     // Build container before test run from Developer powershell (Ctrl+`):
@@ -21,6 +30,15 @@ public class RunningOnOS
     {
         Skip.IfNot(InWindows, "Windows test, skip if in linux environment");
         Assert.True(RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
+    }
+
+    [SkippableFact]
+    public void WindowsVersionTest()
+    {
+        Skip.IfNot(InWindows, "Windows test, skip if in linux environment");
+        Assert.True(RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
+        output.WriteLine(Environment.OSVersion.VersionString);
+
     }
 
 }
